9980de69da7e8145660dfc5196f13ee00d46a572
oveey <ovieigere@gmail.com>
2023-06-24 01:49:13 +0100

    second commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 const Navbar = () => {
+  const[toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div>Navbar</div>
-  )
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo"/>
+        </div>
+        <div class="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibilty">OpenAi</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+
+        <div className="gpt3__navbar-sign">
+          <p>Sign in</p>
+          <button type="button">Sign up</button>
+        </div>
+
+        <div className="gpt3__navbar-menu"></div>
+
+      </div>
+    </div>
+  ) 
 }
 
 export default Navbar
